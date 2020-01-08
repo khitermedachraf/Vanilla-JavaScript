82a01576549eb6d772201396b62767982de1566f
khitermedachraf <hm_khiter@esi.dz>
2020-01-08 18:02:45 +0100

    add some langage javscript  # (Just the begining)

--- test1/teste de base.js
+++ test1/teste de base.js
@@ -1,3 +1,3 @@
 var myVariable = 5.5; // Comme vous pouvez le constater, les nombres à virgule s'écrivent avec un point.
-let myVariable1, myVariable2 = 4,
+var myVariable1, myVariable2 = 4,
     myVariable3;
