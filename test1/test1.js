cf2f8c305d9f590880c52850bac3d2e77022c240
khitermedachraf <hm_khiter@esi.dz>
2020-01-09 01:05:03 +0100

    add some langage javscript  # (Just the begining)

--- test1/test1.js
+++ test1/test1.js
@@ -1,3 +1,2 @@
-var myVariable = 5.5; // Comme vous pouvez le constater, les nombres à virgule s'écrivent avec un point.
-let myVariable1, myVariable2 = 4,
-    myVariable3;
+var myVariable = 5.5; // Comme vous pouvez le constater, les nombres à virgule s'écrivent avec un point.aler 
+alert("HAHAHAHAHA");
--- test1/teste de base.js
+++ test1/teste de base.js
@@ -1,3 +1,14 @@
 var myVariable = 5.5; // Comme vous pouvez le constater, les nombres à virgule s'écrivent avec un point.
-var myVariable1, myVariable2 = 4,
-    myVariable3;
+var myVariable1, myVariable2 = 4;
+var myVariable3;
+var text = 'Ça c\'est quelque chose !';
+print(text);
+var number = 2;
+alert(typeof number); // Affiche : « number »
+
+var text = 'Mon texte';
+alert(typeof text); // Affiche : « string »
+
+var aBoolean = false;
+alert(typeof aBoolean); // Affiche : « boolean »
+alert(typeof nothing); // Affiche : « undefined »
