24760ad2b980f64317378951a6abdeca4f841ddf
khitermedachraf <hm_khiter@esi.dz>
2020-01-10 23:02:08 +0100

    add some langage javscript  # (Just the begining)

--- test1/teste de base.js
+++ test1/teste de base.js
@@ -12,3 +12,7 @@ alert(typeof text); // Affiche : « string »
 var aBoolean = false;
 alert(typeof aBoolean); // Affiche : « boolean »
 alert(typeof nothing); // Affiche : « undefined »
+
+function myFunction(arguments) {
+    // Le code que la fonction va devoir exécuter
+}
