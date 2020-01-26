583609e3ecb1622448bc25348c099165fe1eb2a5
khitermedachraf <hm_khiter@esi.dz>
2020-01-26 20:49:23 +0100

    add some   Javascript Openclassroom  TPs DOM (Just the begining)

--- test1/test2.js
+++ test1/test2.js
@@ -138,3 +138,57 @@ mainDiv.appendChild(defList);
 
 // On insère mainDiv dans le <body>
 document.body.appendChild(mainDiv);
+
+/////////////////////  TP4 *************************** / 
+// On crée l'élément conteneur
+var mainDiv = document.createElement('div');
+mainDiv.id = 'divTP4';
+
+// Création de la structure du formulaire
+var form = document.createElement('form');
+var fieldset = document.createElement('fieldset');
+var legend = document.createElement('legend'),
+    legendText = document.createTextNode('Uploader une image');
+var center = document.createElement('div');
+
+form.action = 'upload.php';
+form.enctype = 'multipart/form-data';
+form.method = 'post';
+
+center.setAttribute('style', 'text-align: center');
+
+legend.appendChild(legendText);
+
+fieldset.appendChild(legend);
+fieldset.appendChild(center);
+
+form.appendChild(fieldset);
+
+// Création des champs
+var label = document.createElement('label'),
+    labelText = document.createTextNode('Image à uploader :');
+var input = document.createElement('input');
+var br = document.createElement('br');
+var submit = document.createElement('input');
+
+input.type = 'file';
+input.id = 'inputUpload';
+input.name = input.id;
+
+submit.type = 'submit';
+submit.value = 'Envoyer';
+
+label.htmlFor = 'inputUpload';
+label.appendChild(labelText);
+
+center.appendChild(label);
+center.appendChild(input);
+center.appendChild(br);
+center.appendChild(br.cloneNode(false)); // On clone, pour mettre un deuxième <br />
+center.appendChild(submit);
+
+// On insère le formulaire dans mainDiv
+mainDiv.appendChild(form);
+
+// On insère mainDiv dans le <body>
+document.body.appendChild(mainDiv);
