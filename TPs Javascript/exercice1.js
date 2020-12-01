16ec90c73a191d6b81996fd9e7eb6918e6c756c3
khiter med achraf <38400387+khitermedachraf@users.noreply.github.com>
2020-12-01 18:35:45 +0100

    Create exercice1.js
    /**
     * ===== Troubleshooting =====
     * The function below should log the number 2, however it does not, 
     * see if you can fix it!
     * Be sure to fix it in the spirit of the code, do not hard code the result.
     */

---
+++ TPs Javascript/exercice1.js
@@ -0,0 +1,22 @@
+/**
+ * ===== Troubleshooting =====
+ * The function below should log the number 2, however it does not, 
+ * see if you can fix it!
+ * Be sure to fix it in the spirit of the code, do not hard code the result.
+ */
+
+function troubleshooting() {
+	let a = 1;
+	const b = 1;
+  let y = "55";
+
+	// Edit between these lines
+	// =================================
+  console.log(parseInt(y));
+	return a + b ;
+
+	// =================================
+}
+
+// Do not change this 
+module.exports = troubleshooting;
