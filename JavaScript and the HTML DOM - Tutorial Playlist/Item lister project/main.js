1be8eb1116656034be28256ae60706e0f573a7af
KHITER Med Achraf <khiter.med7@gmail.com>
2021-03-14 00:08:15 +0100

    add : delete items function

--- JavaScript and the HTML DOM - Tutorial Playlist/Item lister project/main.js
+++ JavaScript and the HTML DOM - Tutorial Playlist/Item lister project/main.js
@@ -4,6 +4,9 @@ var itemList = document.getElementById('items');
 // Form submit event
 form.addEventListener('click', addItem);
 
+// Delete event
+itemList.addEventListener('click', removeItem);
+
 // Add item 
 function addItem(e) {
     e.preventDefault();
@@ -18,6 +21,28 @@ function addItem(e) {
     // Add text node with input value
     li.appendChild(document.createTextNode(newItem));
 
-    itemList.appendChild(li);
 
+    // Create del button element
+    let deleteBtn = document.createElement('button');
+
+    // Add classes to del button
+    deleteBtn.className = 'btn btn-danger btn-sm float-right delete';
+    // Append text node
+    deleteBtn.appendChild(document.createTextNode('X'));
+
+    // Append button to li
+    li.appendChild(deleteBtn);
+
+    // Append li to list   
+    itemList.appendChild(li);
+}
+
+// Remove item
+function removeItem(e) {
+    if (e.target.classList.contains('delete')) {
+        if (confirm('Are You Sure?')) {
+            let li = e.target.parentElement;
+            itemList.removeChild(li);
+        }
+    }
 }
