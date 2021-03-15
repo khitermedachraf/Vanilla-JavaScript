87faf55d79bc62d355291099ed4fa0871251bae2
KHITER Med Achraf <khiter.med7@gmail.com>
2021-03-15 15:50:33 +0100

    add the function : Clear fields

--- MyBookList/main.js
+++ MyBookList/main.js
@@ -29,7 +29,45 @@ class UI {
 
         list.appendChild(row);
     }
+
+    static clearFields() {
+        document.querySelector('#title').value = '';
+        document.querySelector('#author').value = '';
+        document.querySelector('#isbn').value = '';
+    }
 }
+
 
 // Event: Display Books
 document.addEventListener('DOMContentLoaded', UI.displayBooks);
+
+// Event: Add a Book
+document.querySelector('#book-form').addEventListener('submit', (e) => {
+    // Prevent actual submit
+    e.preventDefault();
+
+    // Get form values
+    const title = document.querySelector('#title').value;
+    const author = document.querySelector('#author').value;
+    const isbn = document.querySelector('#isbn').value;
+
+    // Validate
+    if (title === '' || author === '' || isbn === '') {
+        UI.showAlert('Please fill in all fields', 'danger');
+    } else {
+        // Instatiate book
+        const book = new Book(title, author, isbn);
+
+        // Add Book to UI
+        UI.addBookToList(book);
+
+        // // Add book to store
+        // Store.addBook(book);
+
+        // // Show success message
+        // UI.showAlert('Book Added', 'success');
+
+        // Clear fields
+        UI.clearFields();
+    }
+});
